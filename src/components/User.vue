9d2f693f0d72fffb9b1ca5a6890bdce3c23815f6
caolinh22 <48510866+caolinh22@users.noreply.github.com>
2021-06-16 23:27:46 +0700

    update

---
+++ src/components/User.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    {{ titleConverted }} <br />
+    {{ students.length }}
+    <Table :columns="columns" :data="students" />
+  </div>
+</template>
+
+<script>
+import Table from "../components/Table";
+
+export default {
+  name: "Student",
+  components: {
+    Table,
+  },
+  data() {
+    return {
+      title: "user management",
+      students: [],
+      columns: [
+        {
+          label: "Id",
+          field: "id",
+        },
+        {
+          label: "Name",
+          field: "name",
+        },
+        {
+          label: "Username",
+          field: "username",
+        },
+        {
+          label: "Email",
+          field: "email",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.loadStudent();
+  },
+  methods: {
+    loadStudent() {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((data) => (this.students = data));
+    },
+  },
+  computed: {
+    titleConverted() {
+      return this.title.toUpperCase();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <User />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import User from "@/components/User.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    User,
+  },
+};
 </script>
